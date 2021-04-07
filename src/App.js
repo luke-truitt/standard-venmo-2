0cc3608f4a4c5874581439045e4666764e03938f
Luke Truitt <luke@truittluke.com>
2021-04-06 21:15:07 -0600

    ga

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import { Box, ThemeProvider } from "@material-ui/core";
 import Home from "./components/home/Home";
 
 import { v1Theme } from "./utils/styles";
-import ReactGA from 'react-ga';
-const TRACKING_ID = "UA-194068468-1"; // YOUR_OWN_TRACKING_ID
-ReactGA.initialize(TRACKING_ID);
+
 function App() {
   return (
     <ThemeProvider theme={v1Theme}>
